d37e455030425df6b69f495a3e96ade01b12c97c
sonance207 <sonance207@gmail.com>
2017-09-14 22:45:05 -0500

    wildcard redirects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,10 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import {RouterModule, Routes} from '@angular/router';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
+// Routes work from top down
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent, children: [
@@ -22,7 +24,9 @@ const appRoutes: Routes = [
   { path: 'servers', component: ServersComponent, children: [
     { path: ':id', component: ServerComponent},
     { path: ':id/edit', component: EditServerComponent}
-  ]}
+  ]},
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found'}
 
 ];
 
@@ -34,7 +38,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
