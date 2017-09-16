024d14311a2fe4eb2a8b43191e45a3ad593d0428
sonance207 <sonance207@gmail.com>
2017-09-16 16:03:08 -0500

    resolve dynamic data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import {HomeComponent} from './home/home.component';
 import {AuthGuardService} from './auth-guard.service';
 import {CanDeactivateGuardService} from './servers/edit-server/can-deactivate-guard.service';
 import {ErrorPageComponent} from './error-page/error-page.component';
+import {ServerResolverService} from './servers/server/server-resolver.service';
 
 
 // Routes work from top down
@@ -24,7 +25,7 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuardService],
     component: ServersComponent,
     children: [
-    { path: ':id', component: ServerComponent},
+    { path: ':id', component: ServerComponent, resolve: {server: ServerResolverService} },
     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuardService]}
   ]},
 /*  { path: 'not-found', component: PageNotFoundComponent },*/
